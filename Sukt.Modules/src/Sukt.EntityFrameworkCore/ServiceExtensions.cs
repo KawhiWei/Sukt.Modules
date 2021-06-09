5ff7d07d8fdead89e60403d3c3de383dd00819c8
wangzewei <1585955375@qq.com>
2021-06-09 19:46:53 +0800

    modify EntityFramework module

--- Sukt.Modules/src/Sukt.EntityFrameworkCore/ServiceExtensions.cs
+++ Sukt.Modules/src/Sukt.EntityFrameworkCore/ServiceExtensions.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="optionsAction">操作委托</param>
         /// <returns></returns>
 
-        public static IServiceCollection AddSuktDbContext<TDbContext>(this IServiceCollection services, SuktContextOptions dbOption,  Action<IServiceProvider, DbContextOptionsBuilder> optionsAction = null) where TDbContext : SuktDbContextBase
+        public static IServiceCollection AddSuktDbContext<TDbContext>(this IServiceCollection services, Action< SuktContextOptions> dbOption,  Action<IServiceProvider, DbContextOptionsBuilder> optionsAction = null) where TDbContext : SuktDbContextBase
         {
             if(dbOption == null)
             {
@@ -33,13 +33,10 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddHealthChecks().AddDbContextCheck<TDbContext>();
             services.AddDbContext<TDbContext>((provider, builder) =>
             {
+                var option = new SuktContextOptions();
+                dbOption?.Invoke(option);
                 var type = typeof(TDbContext);
-                var settings = provider.GetAppSettings();
-                if (settings == null)
-                {
-                    MessageBox.Show("配置不存在!!");
-                }
-                if (dbOption.MigrationsAssemblyName.IsNullOrEmpty())
+                if (option.MigrationsAssemblyName.IsNullOrEmpty())
                 {
                     MessageBox.Show("迁移程序集名不能为空或null");
                 }
@@ -48,7 +45,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 {
                     MessageBox.Show($"无法找到{type.Name}数据库配置信息!!");
                 }
-                var databaseType = dbOption.DatabaseType;
+                var databaseType = option.DatabaseType;
                 //if (databaseType == Destiny.Core.Flow.Entity.DatabaseType.SqlServer)              
                 //每个类型都要判断。可以使用一个接口，每种类型实现自己的，根据数据类型得到相关驱动，使用（策略模式？工厂模式？？）
                 //配合注入完美
@@ -60,11 +57,11 @@ namespace Microsoft.Extensions.DependencyInjection
                     MessageBox.Show($"没有找到{databaseType}类型的驱动");
                 }
                 DestinyContextOptionsBuilder optionsBuilder1 = new DestinyContextOptionsBuilder();
-                optionsBuilder1.MigrationsAssemblyName = dbOption.MigrationsAssemblyName;
-                var connectionString = dbOption.ConnectionString;
-                if (Path.GetExtension(dbOption.ConnectionString).ToLower() == ".txt") //txt文件
+                optionsBuilder1.MigrationsAssemblyName = option.MigrationsAssemblyName;
+                var connectionString = option.ConnectionString;
+                if (Path.GetExtension(option.ConnectionString).ToLower() == ".txt") //txt文件
                 {
-                    connectionString = provider.GetFileText(dbOption.ConnectionString, $"未找到存放{databaseType.ToDescription()}数据库链接的文件");
+                    connectionString = provider.GetFileText(option.ConnectionString, $"未找到存放{databaseType.ToDescription()}数据库链接的文件");
                 }
                 builder = drivenProvider.Builder(builder, connectionString, optionsBuilder1);
                 optionsAction?.Invoke(provider, builder);
