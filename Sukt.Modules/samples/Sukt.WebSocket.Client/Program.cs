f6fd523623a9275a3457552229e20de0cd1cce51
wangzewei <1585955375@qq.com>
2021-12-07 10:28:50 +0800

    feat : 添加Websocket客户端

--- Sukt.Modules/samples/Sukt.WebSocket.Client/Program.cs
+++ Sukt.Modules/samples/Sukt.WebSocket.Client/Program.cs
@@ -1,74 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Sukt.Module.Core.Extensions;
-using System.Net.WebSockets;
-using System.Text;
+using Sukt.WebSocket.Client;
 
-Console.WriteLine("Hello, World!");
-CancellationTokenSource CTS = new CancellationTokenSource(); ;
-ClientWebSocket clientWebSocket = new ClientWebSocket();
-await clientWebSocket.ConnectAsync(new Uri("ws://localhost:8002/im"), CancellationToken.None);
-Console.WriteLine(clientWebSocket.State);
 
-var msg = new WebSocketReceiveResult() { Id = Guid.NewGuid().ToString(), TargetAction = "im.login", Body = new WebSocketCloseStatusA() { Uid = "asdasdasdasdasdadsada" } };
-
-var replyMess = Encoding.UTF8.GetBytes(msg.ToJson());
-await clientWebSocket.SendAsync(new ArraySegment<byte>(replyMess), WebSocketMessageType.Text, true, CancellationToken.None);
-
-int ReceiveBufferSize  = 8192;
+await Testaaa.SendAsync();
 
 
 
-
-await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
-
-//async Task ReceiveLoop()
-//{
-//    MemoryStream outputStream = null;
-//    WebSocketReceiveResult receiveResult = null;
-//    var buffer = new byte[ReceiveBufferSize];
-//    try
-//    {
-//        while (!CTS.IsCancellationRequested)
-//        {
-
-//            outputStream = new MemoryStream(ReceiveBufferSize);
-//            do
-//            {
-//                receiveResult = await clientWebSocket.ReceiveAsync(buffer, CancellationToken.None);
-//                if (receiveResult.MessageType != WebSocketMessageType.Close)
-//                    outputStream.Write(buffer, 0, receiveResult.Count);
-//            }
-//            while (!receiveResult.EndOfMessage);
-//            if (receiveResult.MessageType == WebSocketMessageType.Close)
-//                outputStream.Position = 0;
-//            ResponseReceived(outputStream);
-//        }
-        
-//    }
-//    catch (TaskCanceledException) { }
-//    finally
-//    {
-//        outputStream?.Dispose();
-//    }
-//}
-//void ResponseReceived(Stream inputStream)
-//{
-//    // TODO: handle deserializing responses and matching them to the requests.
-//    // IMPORTANT: DON'T FORGET TO DISPOSE THE inputStream!
-//}
 while (true)
 {
     await Task.Delay(1000);
 }
 
 
-public class WebSocketReceiveResult
-{
-    public string Id { get; set; }
-    public string TargetAction { get; set; }
-    public WebSocketCloseStatusA Body { get; set; }
-}
-public class WebSocketCloseStatusA
-{
-    public string Uid { get; set; } 
-}
---
+++ Sukt.Modules/samples/Sukt.WebSocket.Client/Testaaa.cs
@@ -0,0 +1,62 @@
+﻿using Sukt.Module.Core.Extensions;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Sukt.WebSocket.Client
+{
+    public class Testaaa
+    {
+        private static ClientWebSocket clientWebSocket;
+        private static CancellationTokenSource CTS;
+        static int ReceiveBufferSize = 8 * 1024;
+        public static async Task SendAsync()
+        {
+            CTS = new CancellationTokenSource();
+            Console.WriteLine("Hello, World!");
+            clientWebSocket = new ClientWebSocket();
+            await clientWebSocket.ConnectAsync(new Uri("ws://localhost:8002/im"), CancellationToken.None);
+            Console.WriteLine(clientWebSocket.State);
+
+            var msg = new { id = Guid.NewGuid().ToString(), targetAction = "im.login", body = new { uid = "asdasdasdasdasdadsada" } };
+            var replyMess = Encoding.UTF8.GetBytes(msg.ToJson());
+            await clientWebSocket.SendAsync(new ArraySegment<byte>(replyMess), WebSocketMessageType.Text, true, CancellationToken.None);//监听Socket信息
+            //全部消息容器
+            List<byte> bs = new List<byte>();
+            //缓冲区
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            //是否关闭
+            while (!result.CloseStatus.HasValue)
+            {
+                //文本消息
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    bs.AddRange(buffer.Take(result.Count));
+                    //消息是否已接收完全
+                    if (result.EndOfMessage)
+                    {
+                        //发送过来的消息
+                        string userMsg = Encoding.UTF8.GetString(bs.ToArray(), 0, bs.Count);
+                        Console.WriteLine(userMsg);
+                        //清空消息容器
+                        bs = new List<byte>();
+                    }
+                    //继续监听Socket信息
+                    result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+            }
+
+
+        }
+        public class WebSocketReceive
+        {
+            public string Id { get; set; }
+            public string TargetAction { get; set; }
+            public object Body { get; set; }
+        }
+        public class WebSocketCloseStatusA
+        {
+            public string Uid { get; set; }
+        }
+    }
+}
