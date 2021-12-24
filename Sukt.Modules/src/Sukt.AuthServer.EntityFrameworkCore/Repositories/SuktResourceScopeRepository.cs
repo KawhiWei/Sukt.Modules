4fd3ab4c3acdbc6fc3f05f844a2698c10639389d
wangzewei <1585955375@qq.com>
2021-12-24 22:00:58 +0800

    feat : 添加测试微软yarp做网关转发demo，添加认证服务模块应用层，添加认证资源仓储

---
+++ Sukt.Modules/src/Sukt.AuthServer.EntityFrameworkCore/Repositories/SuktResourceScopeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sukt.AuthServer.Domain.Aggregates.SuktResourceScopes;
+using Sukt.AuthServer.Domain.Repositories;
+using Sukt.EntityFrameworkCore;
+
+namespace Sukt.AuthServer.EntityFrameworkCore.Repositories
+{
+    public class SuktResourceScopeRepository : AggregateRootBaseRepository<SuktResourceScope, string>, ISuktResourceScopeRepository
+    {
+        public SuktResourceScopeRepository(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public virtual async Task<IReadOnlyCollection<SuktResourceScope>> FindResourcesByScopeAsync(IEnumerable<string> scopeNames)
+        {
+            return await TrackEntities.Where(x => x.Resources.Any(a => scopeNames.Contains(a))).ToListAsync();
+        }
+    }
+}
